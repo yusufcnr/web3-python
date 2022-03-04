bb1b0bc6f8b364e63beed06b65a4d170ffde8fff
Yusuf Cinar <75760010+yusufcnr@users.noreply.github.com>
2022-03-04 15:52:48 +0300

    Update web3script.py

--- web3script.py
+++ web3script.py
@@ -3,6 +3,6 @@ infura_url = "https://mainnet.infura.io/v3/a85f2f1e78ba427ab6120e60d4ee92e3"
 web3 = Web3(Web3.HTTPProvider(infura_url))
 conStatus = web3.isConnected()
 print(conStatus)
-ethWallet = "0xCC072bBE1BC8c4Fd8f901f7CaE344266917d2FEf"
+ethWallet = "ETH_ADDRESS"
 balance = web3.eth.getBalance(ethWallet)
 print(balance)
